f97932f5e1fede0130766762be4da5d1d4c48df6
ProstoEde <43632106+ProstoEde@users.noreply.github.com>
2019-01-25 21:24:24 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.prostoede.test;
+
+/**
+ *
+ * @author prostoede
+ */
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            System.out.println("Hello!!!");
+        }
+    }
+}
